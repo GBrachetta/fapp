2d9ae888fce18b29c24594df0f8ff016a073e392
Guillermo Brachetta <brachetta@me.com>
2020-04-22 19:22:12 +0200

    Base app

---
+++ app.py
@@ -0,0 +1,92 @@
+import os
+from flask import Flask, render_template, url_for, request, session, redirect, flash, g
+from flask_pymongo import PyMongo
+import bcrypt
+from functools import wraps
+from os import path
+if path.exists('env.py'):
+    import env
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+COLLECTION_NAME = 'users'
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+# About
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Check if user logged in
+
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorised, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+# Login form
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+# Dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
+
+# Login process
+@app.route('/authenticate', methods=['POST', 'GET'])
+def authenticate():
+    users = mongo.db.users
+    login_user = users.find_one({'name': request.form['username']})
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
+            session['username'] = request.form['username']
+            session['logged_in'] = True
+            flash('You are now logged in', 'success')
+            return redirect(url_for('dashboard'))
+    flash('Invalid username/password combination', 'danger')
+    return redirect(url_for('login'))
+
+# Register
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name': request.form['username']})
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(
+                request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert(
+                {'name': request.form['username'], 'password': hashpass})
+            session['username'] = request.form['username']
+            flash('You are now registered and can log in', 'success')
+            return redirect(url_for('login'))
+        flash('That username already exists!', 'danger')
+    return render_template('register.html')
+
+
+if __name__ == "__main__":
+    # When debug is set to True, any error messages are described when the page is loaded
+    app.run(debug=True)
